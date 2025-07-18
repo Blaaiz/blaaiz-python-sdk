73f40fefee153609f257e6a8f0d6017076816b29
gbxnga <iamblizzyy@gmail.com>
2025-07-18 16:23:29 +0100

    updated python version

--- setup.py
+++ setup.py
@@ -28,7 +28,6 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
@@ -39,7 +38,7 @@ setup(
         "Topic :: Office/Business :: Financial",
     ],
     packages=find_packages(),
-    python_requires=">=3.7",
+    python_requires=">=3.8",
     install_requires=[
         # No external dependencies - uses only Python standard library
     ],
